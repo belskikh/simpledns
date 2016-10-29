2023406b31f3cd2e4859f5d0e3cc9b3412fa74bd
Belskikh <belskikh.aleksandr@gmail.com>
2016-10-29 12:14:36 +0300

    created simple resource record

--- server.go
+++ server.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/miekg/dns"
+	"net"
 )
 
 func main() {
-	fmt.Println("Let's play some DNS!!!11")
+	// let's create a simple A resource record
+	r := new(dns.A)
+	r.Hdr = dns.RR_Header{Name: "8level.ru.", Rrtype: dns.TypeA,
+		Class: dns.ClassINET, Ttl: 300}
+	r.A = net.IPv4(185, 37, 61, 185)
+	// testing value
+	fmt.Println(r)
 }
