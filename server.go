303a01cdc5f879c222687d242403d5a8437f3469
Belskikh <belskikh.aleksandr@gmail.com>
2016-10-29 13:32:16 +0300

    wtf is going in?

--- server.go
+++ server.go
@@ -6,6 +6,21 @@ import (
 	"net"
 )
 
+func handlerRequest(w dns.ResponseWriter, r *dns.Msg) {
+	m := new(dns.Msg)
+	m.SetReply(r)
+	// if r.IsTsig() != nil {
+	// 	if w.TsigStatus() == nil {
+	// 		// *Msg r has an TSIG record and it was validated
+	// 		m.SetTsig("axfr.", dns.HmacMD5, 300, time.Now().Unix())
+	// 	} else {
+	// 		// *Msg r has an TSIG records and it was not valided
+	// 	}
+	// }
+	w.WriteMsg(m)
+	fmt.Println("hello!")
+}
+
 func main() {
 	// let's create a simple A resource record
 	r := new(dns.A)
@@ -21,24 +36,5 @@ func main() {
 
 	server := &dns.Server{Addr: ":53", Net: "upd"}
 	go server.ListenAndServe()
-
-	handlerRequest := func(w dns.ResponseWriter, r *dns.Msg) {
-		m := new(dns.Msg)
-		m.SetReply(r)
-		// if r.IsTsig() != nil {
-		// 	if w.TsigStatus() == nil {
-		// 		// *Msg r has an TSIG record and it was validated
-		// 		m.SetTsig("axfr.", dns.HmacMD5, 300, time.Now().Unix())
-		// 	} else {
-		// 		// *Msg r has an TSIG records and it was not valided
-		// 	}
-		// }
-		w.WriteMsg(m)
-	}
-
 	dns.HandleFunc(".", handlerRequest)
-
-	// testing values
-	fmt.Println(r)
-	fmt.Println(m)
 }
