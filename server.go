0b449ac6935056e3a3adc5b70322ee14c204b7a3
Belskikh <belskikh.aleksandr@gmail.com>
2016-10-29 13:13:20 +0300

    start playing with server

--- server.go
+++ server.go
@@ -16,6 +16,28 @@ func main() {
 	// create a simple message
 	m := new(dns.Msg)
 	m.SetQuestion("8level.ru.", dns.TypeA)
+
+	// creat and start server
+
+	server := &dns.Server{Addr: ":53", Net: "upd"}
+	go server.ListenAndServe()
+
+	handlerRequest := func(w dns.ResponseWriter, r *dns.Msg) {
+		m := new(dns.Msg)
+		m.SetReply(r)
+		// if r.IsTsig() != nil {
+		// 	if w.TsigStatus() == nil {
+		// 		// *Msg r has an TSIG record and it was validated
+		// 		m.SetTsig("axfr.", dns.HmacMD5, 300, time.Now().Unix())
+		// 	} else {
+		// 		// *Msg r has an TSIG records and it was not valided
+		// 	}
+		// }
+		w.WriteMsg(m)
+	}
+
+	dns.HandleFunc(".", handlerRequest)
+
 	// testing values
 	fmt.Println(r)
 	fmt.Println(m)
