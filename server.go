ceb8688c01f7cfd7e82d2b0feaf8f66e0840e9a6
Belskikh <belskikh.aleksandr@gmail.com>
2016-10-29 18:58:18 +0300

    added request handling

--- server.go
+++ server.go
@@ -3,35 +3,51 @@ package main
 import (
 	"fmt"
 	"github.com/miekg/dns"
-	"net"
+	// "net"
+	// "reflect"
 )
 
+func serve() {
+	server := &dns.Server{Addr: ":53", Net: "udp"}
+
+	err := server.ListenAndServe()
+	defer server.Shutdown()
+
+	if err != nil {
+		fmt.Printf("Error - %s", err)
+	}
+}
+
+// func handleQuery(m *dns.Msg) {
+// 	var rr dns.RR
+
+// }
+
 func handleRequest(w dns.ResponseWriter, r *dns.Msg) {
+
+	records := make(map[string]dns.RR)
+
+	rr, _ := dns.NewRR("8level.ru. 300 IN A 185.37.61.185")
+
+	records["8level.ru."] = rr
+
+	rr2, _ := dns.NewRR("panel.8level.ru. 300 IN A 185.37.61.185")
+	records["panel.8level.ru."] = rr2
+
 	m := new(dns.Msg)
 	m.SetReply(r)
+	m.Answer = append(m.Answer, records["8level.ru."])
+	fmt.Println(records["8level.ru."])
 	w.WriteMsg(m)
 }
 
 func main() {
-	// let's create a simple A resource record
-	r := new(dns.A)
-	r.Hdr = dns.RR_Header{Name: "8level.ru.", Rrtype: dns.TypeA,
-		Class: dns.ClassINET, Ttl: 300}
-	r.A = net.IPv4(185, 37, 61, 185)
-
-	// create a simple message
-	m := new(dns.Msg)
-	m.SetQuestion("8level.ru.", dns.TypeA)
 
 	// create and start server
-
-	server := &dns.Server{Addr: ":8080", Net: "udp"}
-
 	dns.HandleFunc(".", handleRequest)
 
-	err := server.ListenAndServe()
+	go serve()
 
-	if err != nil {
-		fmt.Println(err)
-	}
+	var input string
+	fmt.Scanln(&input)
 }
