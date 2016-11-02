40094a0109f24c32c93e4aca54c0661712ddb122
Belskikh <belskikh.aleksandr@gmail.com>
2016-11-02 21:56:33 +0300

    add recursive requesting

--- server.go
+++ server.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"log"
 	"github.com/kirillDanshin/myutils"
 	"github.com/miekg/dns"
+	"log"
 )
 
 // init sample storage to test server
@@ -25,7 +25,7 @@ func init() {
 func serve(net string) {
 	server := &dns.Server{Addr: ":53", Net: net}
 	defer server.Shutdown()
-	
+
 	err := server.ListenAndServe()
 
 	if err != nil {
@@ -34,14 +34,42 @@ func serve(net string) {
 	}
 }
 
+func getExternalRecord(r *dns.Msg) []dns.RR {
+
+	// creating new message for sending to external server
+	question := r.Question[0]
+	m := new(dns.Msg)
+	m.SetQuestion(question.Name, dns.TypeA)
+
+	// hardcoded external server adress
+	servAdress := "8.8.8.8:53"
+
+	// uncomment these lines to get advanced features
+	// c := new(dns.Client)
+	// in, rtt, err := c.Exchange(m, servAdress)
+
+	// simple UDP request. comment if using dns.Client
+	in, err := dns.Exchange(m, servAdress)
+	myutils.LogFatalError(err)
+
+	return in.Answer
+
+}
+
 func handleRequest(w dns.ResponseWriter, r *dns.Msg) {
 
 	m := new(dns.Msg)
 	m.SetReply(r)
 
+	// question looks like this {[...]}
 	for _, q := range r.Question {
 		if record, ok := records[q.Name]; ok {
 			m.Answer = append(m.Answer, record)
+		} else {
+			externalRecord := getExternalRecord(r) // type []dns.RR
+			for _, record := range externalRecord {
+				m.Answer = append(m.Answer, record)
+			}
 		}
 	}
 
